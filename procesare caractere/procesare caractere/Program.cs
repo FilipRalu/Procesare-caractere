759c73af9f4841554bea66f8714081bb3a98b150
Raluca-Andrada Filip <filip.ralucaandrada@student.uoradea.ro>
2022-06-22 00:24:12 +0300

    procesare

---
+++ procesare caractere/procesare caractere/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace procesare_caractere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numele fisierului dorit:");
+            Procesare procesare = new Procesare();
+            Console.WriteLine("In fisierul ales sunt: {0} vocale.", procesare.Vocale);
+            Console.WriteLine("In fisierul ales sunt: {0} consoane.", procesare.Consoane);
+            Console.WriteLine("In fisierul ales sunt: {0} linii.", procesare.Linii);
+            Console.WriteLine("In fisierul ales sunt: {0} caractere.", procesare.Total);
+
+        }
+    }
+}
